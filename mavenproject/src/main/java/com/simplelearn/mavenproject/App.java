0792df09d3570e96a12dfd2a4a7e07f522783aee
Jens Rasmussen <fjallatind@gmail.com>
2020-12-26 16:14:38 +0100

    Test

---
+++ mavenproject/src/main/java/com/simplelearn/mavenproject/App.java
@@ -0,0 +1,14 @@
+package com.simplelearn.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// Some comment
+        System.out.println( "Hello World!" );
+    }
+}
