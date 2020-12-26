aa618580757cb80864f11731917c1033b7c1592a
Jens Rasmussen <fjallatind@gmail.com>
2020-12-26 16:45:03 +0100

    test

---
+++ maven2/src/main/java/com/simplelearn/maven2/App.java
@@ -0,0 +1,13 @@
+package com.simplelearn.maven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
